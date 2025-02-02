47e5518dc83b0895479356eb5cc7d42f235b93c9
Sajmir Doko <sajdoko@gmail.com>
2025-02-02 17:29:52 +0100

    .

--- wp-content/themes/weather-app-theme/index.php
+++ wp-content/themes/weather-app-theme/index.php
@@ -2,8 +2,8 @@
 get_header(); // Include the header
 ?>
 <main class="flex-shrink-0">
-    <div class="container mt-5">
-        <h1 class="text-center mb-4">Weather App</h1>
+    <div class="container mt-3">
+        <h1 class="text-center mb-4">Cities Weather Data</h1>
         <div class="row">
             <div class="col-12">
                 <!-- Display weather data using the shortcode -->
